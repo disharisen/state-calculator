b8988195ec844048dc080909ff11102ed49287cf
Dishari Sen <ms.disharisen@gmail.com>
2025-12-27 14:01:04 +0530

    In Basic Calculator connect state with Input and add Function with button .

--- src/components/BasicCalculator.tsx
+++ src/components/BasicCalculator.tsx
@@ -1,30 +1,70 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "./shadcnui/button";
 import { Input } from "./shadcnui/input";
 
 const BasicCalculator = () => {
+	const [InputOne, setInputOne] = useState("");
+	const [InputTwo, setInputTwo] = useState("");
+	const [InputTotal, setInputTotal] = useState("");
+
+	const handleAdd = () => {};
+
+	const handleMinus = () => {};
+
+	const handleMultiply = () => {};
+
+	const handleDivide = () => {};
+
+	const handleReset = () => {};
+
 	return (
 		<div className="grid grid-cols-4 gap-8">
 			<Input
 				className="col-span-2"
 				placeholder="Number 1"
+				onChange={({ target }) => setInputOne(target.value)}
+				value={InputOne}
 			/>
 			<Input
 				className="col-span-2"
 				placeholder="Number 2"
+				onChange={({ target }) => setInputTwo(target.value)}
+				value={InputTwo}
 			/>
 			<Input
 				className="col-span-4"
 				placeholder="Total"
 				disabled
+				onChange={({ target }) => setInputTotal(target.value)}
+				value={InputTotal}
 			/>
 
-			<Button className="col-span-1 cursor-pointer">Add</Button>
-			<Button className="col-span-1 cursor-pointer">Subtract</Button>
-			<Button className="col-span-1 cursor-pointer">Multiply</Button>
-			<Button className="col-span-1 cursor-pointer">Divide</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				onClick={handleAdd}>
+				Add
+			</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				onClick={handleMinus}>
+				Subtract
+			</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				onClick={handleMultiply}>
+				Multiply
+			</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				onClick={handleDivide}>
+				Divide
+			</Button>
 			<Button
 				className="col-span-4 cursor-pointer"
-				variant={"destructive"}>
+				variant={"destructive"}
+				onClick={handleReset}>
 				Reset
 			</Button>
 		</div>
