0408babe90b9d33414720089b0b5dcd85c0ee633
Dishari Sen <ms.disharisen@gmail.com>
2026-01-03 13:45:49 +0530

    Advance Calculator Reset Function Done .

--- src/components/AdvanceCalculator.tsx
+++ src/components/AdvanceCalculator.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { DivideIcon, MinusIcon, PlusIcon, XIcon } from "lucide-react";
+import { useState } from "react";
 import { Button } from "./shadcnui/button";
 import { Input } from "./shadcnui/input";
 import {
@@ -10,14 +13,28 @@ import {
 } from "./shadcnui/select";
 
 const AdvanceCalculator = () => {
+	const [inputOne, setInputOne] = useState("");
+	const [inputTwo, setInputTwo] = useState("");
+	const [inputTotal, setInputTotal] = useState("");
+
+	const [inputOperator, setInputOperator] = useState("");
+
+	const handleCalculate = () => {};
+
+	const handleReset = () => {};
+
 	return (
 		<div className="grid grid-cols-3 gap-8">
 			<Input
 				className="col-span-1"
 				placeholder="Number 1"
+				onChange={({ target }) => setInputOne(target.value)}
+				value={inputOne}
 			/>
 
-			<Select>
+			<Select
+				onValueChange={(value) => setInputOperator(value)}
+				value={inputOperator}>
 				<SelectTrigger className="w-full">
 					<SelectValue placeholder="Operator" />
 				</SelectTrigger>
@@ -38,19 +55,37 @@ const AdvanceCalculator = () => {
 			</Select>
 
 			<Input
+				type="number"
 				className="col-span-1"
 				placeholder="Number 2"
+				onChange={({ target }) => setInputTwo(target.value)}
+				value={inputTwo}
 			/>
 
 			<Input
+				type="number"
 				className="col-span-2"
 				placeholder="Total"
 				disabled
+				onChange={({ target }) => setInputTotal(target.value)}
+				value={inputTotal}
 			/>
 
-			<Button className="col-span-1 cursor-pointer">Calculate</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				disabled={inputOne === "" || inputTwo === "" || inputOperator === ""}
+				onClick={handleCalculate}>
+				Calculate
+			</Button>
 			<Button
 				className="col-span-3 cursor-pointer"
+				disabled={
+					inputOne === "" &&
+					inputTwo === "" &&
+					inputOperator === "" &&
+					inputTotal === ""
+				}
+				onClick={handleReset}
 				variant={"destructive"}>
 				Reset
 			</Button>
